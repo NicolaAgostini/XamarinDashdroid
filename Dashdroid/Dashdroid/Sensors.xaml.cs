d8986c0c1cdebef6fe4976e767963b784a6a92c4
Nicola Nico <nicolanico@iMac-di-Nicola.local>
2018-12-18 18:50:21 +0100

    gyroscope battery gps and magnetometer

--- Dashdroid/Dashdroid/Sensors.xaml.cs
+++ Dashdroid/Dashdroid/Sensors.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace Dashdroid
 {
-    public static class StringExt
+    public static class StringExt //classe per troncare le stringhe (i valori dell'accelerometro troppo lungnhi)
     {
         public static string Truncate(this string value, int maxLength)
         {
@@ -20,31 +20,95 @@ namespace Dashdroid
     public partial class Sensors : ContentPage
     {
          SensorSpeed speed = SensorSpeed.UI;
-        ObservableCollection<SensorDef> SensorDatas = new ObservableCollection<SensorDef>();
 
         public Sensors()
         {
             InitializeComponent();
-            SersorView.ItemsSource = SensorDatas;
-            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
-            SensorDatas.Add(new SensorDef { DisplayValue = "Accelerometer" });
+
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged; //Registrazione all'evento accelerometro
+
+
+            Battery.BatteryInfoChanged += Battery_BatteryChanged; //Registrazione evento batteria
+            var level = Battery.ChargeLevel;
+      
+            var source = Battery.PowerSource;
+
+            Battery_value.Text = "Battery level: " + level*100 + 
+            " Battery Power Source: " + source;
+            RetrievePosition();
+
+            Gyroscope.ReadingChanged += Gyroscope_ReadingChanged; //Registrazione evento giroscopio
+
+            Magnetometer.ReadingChanged += Magnetometer_ReadingChanged; // Registrazione all'evento magnetometro
+
         }
 
-        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+
+
+       async void RetrievePosition()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+            var location = await Geolocation.GetLocationAsync(request);
+
+            if (location != null)
+            {
+                //Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                GPS_value.Text = "Latitude: "+ location.Latitude +Environment.NewLine
+                + "Longitude: "+location.Longitude + Environment.NewLine
+                     + " Altitude: "+location.Altitude;
+            }
+        }
+
+        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e) //gestisco l'evento accelerometro
          {
         var data = e.Reading;
             // Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
-            SensorDatas.Add(new SensorDef { DisplayValue = "X: " + data.Acceleration.X.ToString().Truncate(3) +
-             " Y: " + data.Acceleration.Y.ToString().Truncate(3) + 
-                " Z: " + data.Acceleration.Z.ToString().Truncate(3)
-            });
+          
 
-            SensorDatas.Add(new SensorDef { DisplayValue = " " });
+           
 
-            //Accelerometer_value.Text = "X: "+ data.Acceleration.X + " Y: " + data.Acceleration.Y + " Z: " + data.Acceleration.Z;
+            Accelerometer_value.Text = "X: "+ data.Acceleration.X.ToString().Truncate(4) + 
+            " Y: " + data.Acceleration.Y.ToString().Truncate(4) +
+                 " Z: " + data.Acceleration.Z.ToString().Truncate(4);
             // Process Acceleration X, Y, and Z
         }
-private void ToggleAccelerometer_OnToggled(object sender, ToggledEventArgs e)
+
+
+        void Battery_BatteryChanged(object sender, BatteryInfoChangedEventArgs e)  //gestisco l'evento della batteria
+        {
+            var level = e.ChargeLevel;
+            var state = e.State;
+            var source = e.PowerSource;
+            //Console.WriteLine($"Reading: Level: {level}, State: {state}, Source: {source}");
+            Battery_value.Text = "Battery level: "+ level*100+ " Battery Power Source: " + source;
+
+        }
+
+        void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)  //gestisco l'evento giroscopio
+        {
+            var data = e.Reading;
+            // Process Angular Velocity X, Y, and Z reported in rad/s
+            //Console.WriteLine($"Reading: X: {data.AngularVelocity.X}, Y: {data.AngularVelocity.Y}, Z: {data.AngularVelocity.Z}");
+
+            Gyroscope_value.Text = "X: " + data.AngularVelocity.X.ToString().Truncate(5) +
+             " Y: "+ data.AngularVelocity.Y.ToString().Truncate(5) +
+                 " Z: "+ data.AngularVelocity.Y.ToString().Truncate(5);
+        }
+
+        void Magnetometer_ReadingChanged(object sender, MagnetometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process MagneticField X, Y, and Z
+            //Console.WriteLine($"Reading: X: {data.MagneticField.X}, Y: {data.MagneticField.Y}, Z: {data.MagneticField.Z}");
+            Magnetometer_value.Text = "X: " + data.MagneticField.X.ToString().Truncate(2) +
+             " Y: " + data.MagneticField.Y.ToString().Truncate(2) +
+                 " Z: " + data.MagneticField.Y.ToString().Truncate(2);
+        }
+
+
+
+
+        private void ToggleAccelerometer_OnToggled(object sender, ToggledEventArgs e) //non è detto mi serva magari lo metto nella pagina settings
     {
         try
         {
@@ -62,6 +126,44 @@ private void ToggleAccelerometer_OnToggled(object sender, ToggledEventArgs e)
             // Other error has occurred.
         }
     }
+
+        public void ToggleGyroscope_OnToggled(object sender, ToggledEventArgs e)
+        {
+            try
+            {
+                if (Gyroscope.IsMonitoring)
+                    Gyroscope.Stop();
+                else
+                    Gyroscope.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+        public void ToggleMagnetometer_OnToggled(object sender, ToggledEventArgs e)
+        {
+            try
+            {
+                if (Magnetometer.IsMonitoring)
+                    Magnetometer.Stop();
+                else
+                    Magnetometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
 
 
 
